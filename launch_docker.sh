38e76ef416bf487b82882e49d69a04ad336a54ce
Hoosang Lee <lhs223@gmail.com>
2022-09-07 10:38:00 +0900

    fixed docker container name

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,6 +2,6 @@ sudo docker run --gpus all -it --net=host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v `pwd`/share:/root/share \
-    --name test \
+    --name core \
     lhs223/core_container \
     bash
