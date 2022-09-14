4a822e04bc044172bd4ab7c3576b59120ce995d1
Hoosang Lee <lhs223@gmail.com>
2022-09-14 10:29:45 +0900

    Update launch_docker.sh
    update working directory

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,6 +2,7 @@ sudo docker run --gpus all -it --net=host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v `pwd`/share:/root/share \
+    -w /root/share \
     --name core \
     lhs223/core_container \
     bash
